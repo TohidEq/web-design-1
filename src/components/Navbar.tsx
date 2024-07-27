6730891da55f734514df90ba0623cd989f1aa541
TohidEq <TohidEq@gmail.com>
2024-07-27 21:55:08 +0330

    Just Create Navbar and  Welcome files

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function Navbar({}: Props) {
+  return <div className="navbar">NT. - search - menu</div>;
+}
+
+export default Navbar;
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,9 @@
+type Props = {};
+
+export default function Welcome({}: Props) {
+  return (
+    <div className="w-full h-[200vh] bg-green-800 text-white text-center">
+      Welcome
+    </div>
+  );
+}
